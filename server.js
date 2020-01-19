52421e1e4ff9f4b1e51248d31a481e691542fa2d
Mike Neilens <mike.neilens@gmail.com>
2020-01-19 15:37:45 +0000

    added app.js to server.js

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
 
+const challenge20 = require('app.js');
+
 app.get('/', (req, res) => {
   const name = req.query.name;
-  res.send('Hello ' + name + ' from App Engine!');
+  //res.send('Hello ' + name + ' from App Engine!');
+  res.send( challenge20.getInitialTrolley() );
 });
 
 // Listen to the App Engine-specified port, or 8080 otherwise
