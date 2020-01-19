6eede132b8fca32f97f76583a160cdf0fbaf4ae0
Mike Neilens <mike.neilens@gmail.com>
2020-01-19 15:33:24 +0000

    complete app.js

--- app.js
+++ app.js
@@ -100,4 +100,75 @@ const startingPosition = (mapData) => {
     } 
 } 
 
+class Trolley {
+    constructor(position,orientation) {
+        this.position = position;
+        this.orientation = orientation;
+    }
+    get positionAhead() {
+      return this.position.plus(this.orientation.ahead);
+    }
+    move() {
+      return new Trolley(this.positionAhead,this.orientation);
+    }
+    rotateLeft() {
+      return new Trolley(this.position, this.orientation.turnLeft);
+    }
+    rotateRight() {
+      return new Trolley(this.position, this.orientation.turnRight)
+    }
+    viewAhead(mapData) {
+      return viewAheadAt(mapData, this.position, this.orientation);
+    }
+    moveOrRotate(command,mapData) {
+      switch(command) {
+        case "M": if (contentAt(mapData, this.positionAhead) != fixture) {
+          return this.move();
+        } else {
+          return this;
+        }
+        case "L": return this.rotateLeft();
+        case "R": return this.rotateRight();
+      }
+    }
+    encode() {
+      const string = this.position.x.toString() + "," + this.position.y.toString() + "," + this.orientation.value;
+      let buff = new Buffer.from(string);
+      return buff.toString('base64');
+    }
+    refernceIdandViewAhead(mapData) {
+        let output = this.encode();
+        for (const element of this.viewAhead(mapData)) {
+          output = output + "," + element;
+        } 
+        return output;
+    }
+}
 
+const trolleyFrom = (referenceId) => {
+      const buff = new Buffer.from(referenceId, 'base64');
+      const decoded =  buff.toString('ascii');
+      const parts = decoded.split(",");
+      return new Trolley(new Position(Number(parts[0]),Number(parts[1])), new Orientation(parts[2])  );
+}
+
+const map = "*******" + newLine + "**    *" + newLine + "** ** *";
+
+const getInitialTrolley = () => {
+  return new Trolley(startingPosition(map),new Orientation("E"));
+}
+
+const map = "*******" + newLine + "**    *" + newLine + "** ** *";
+
+const getInitialTrolley = () => {
+  const trolley = new Trolley(startingPosition(map),new Orientation("E"));
+  return trolley.viewAheadAndReferenceId(mapData);
+}
+
+const moveTrolley = (command, referenceId, mapData) => {
+    const trolley = trolleyFrom(referenceId);
+    const movedTrolley = trolley.moveOrRotate(command, mapData);
+    return movedTrolley.viewAheadAndReferenceId(mapData);
+}
+
+module.exports = {getInitialTrolley, movedTrolley};
